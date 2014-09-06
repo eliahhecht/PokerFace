dace4d54d121b0689780fe2b7ceb97a3ee8538c0
Eliah Hecht <eliahhecht@gmail.com>
2014-09-06 19:33:59 -0400

    start work on ranking

--- src/main/scala/pokerface/Hand.scala
+++ src/main/scala/pokerface/Hand.scala
@@ -1,9 +1,36 @@
 package pokerface
 
+import scala.collection.concurrent.RDCSS_Descriptor
+
 /**
  * Created by Eliah on 9/6/2014.
  */
 class Hand(val cards: Set[Card]) {
+
+  def hasFullHouse: Boolean = {
+    val groupedByRank = cards.groupBy(_.rank)
+    val hasPair = groupedByRank.exists(_._2.size == 2)
+    val hasThreeOfAKind = groupedByRank.exists(_._2.size == 3)
+
+    return hasPair && hasThreeOfAKind
+  }
+
+  def hasJacksOrBetterPair: Boolean = {
+    val groupedByRank = cards.groupBy(_.rank)
+    return groupedByRank.exists(g => g._2.size == 2 && g._1 >= 11)
+  }
+
+  def getType = {
+    if (hasFullHouse) {
+      HandType.FullHouse
+    }
+    else if (hasJacksOrBetterPair) {
+      HandType.JacksOrBetter
+    }
+    else {
+      HandType.Nothing
+    }
+  }
 
   if (cards.size != 5) {
     throw new IllegalArgumentException("Number of cards must be exactly 5")
@@ -11,7 +38,7 @@ class Hand(val cards: Set[Card]) {
 
   override def toString = cards.mkString(",")
 
-  override def equals(obj: Any) : Boolean = {
+  override def equals(obj: Any): Boolean = {
     if (!(obj.isInstanceOf[Hand])) {
       return false
     } else {
@@ -39,6 +66,7 @@ object Hand {
 
 object HandType extends Enumeration {
   type HandType = Value
-  val JacksOrBetter, TwoPair, ThreeOfAKind, Straight, Flush, FullHouse, FourOfAKind, StraightFlush, RoyalFlush = Value
+  val Nothing, JacksOrBetter, TwoPair, ThreeOfAKind, Straight, Flush, FullHouse, FourOfAKind, StraightFlush, RoyalFlush
+  = Value
 }
 
--- src/test/scala/pokerface/HandTypingSpec.scala
+++ src/test/scala/pokerface/HandTypingSpec.scala
@@ -1,16 +1,28 @@
 package pokerface
 
-import org.scalatest.FlatSpec
+import org.scalatest.prop.TableDrivenPropertyChecks
+import org.scalatest.{Matchers, FlatSpec}
+import pokerface.HandType.HandType
 
 /**
  * Created by Eliah on 9/6/2014.
  */
-class HandTypingSpec extends FlatSpec{
+class HandTypingSpec extends FlatSpec with Matchers with TableDrivenPropertyChecks{
 
   behavior of "pokerface.Hand"
 
-  it should "score a jacks-or-better pair as jacks or better" in {
-    val hand = Hand.parse("jd js 1d 2c 3s").get
-  }
+  val hands = Table(
+    ("hand", "expected rank"),
+    ("jd js 1d 2c 3s", HandType.JacksOrBetter),
+    ("qd qs 1d 2c 3s", HandType.JacksOrBetter),
+    ("qd js 1d 2c 3s", HandType.Nothing),
+    ("1d 3c 5s 7d jd", HandType.Nothing),
+    ("ac ah ad 3d 3s", HandType.FullHouse)
+  )
 
+  it should "rank hands correctly" in forAll(hands) {(handString: String, expectedRank: HandType) =>
+    val hand = Hand.parse(handString)
+    assume(hand.isDefined)
+    hand.get.getType shouldEqual expectedRank
+  }
 }
